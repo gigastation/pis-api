4662019f2de268c89ec5d30842f5e7889614888a
kyokoshima <dev.kyoko@gmail.com>
2020-03-22 18:07:07 +0900

    Complete implementation of get product for now

--- api/models/product.py
+++ api/models/product.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.utils import timezone
+import requests
+import environ
+import base64
+from .category import Category
 
-# Create your models here.
-
-
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-
+env = environ.Env()
 
 class Product(models.Model):
     product_id = models.IntegerField()
@@ -23,11 +22,11 @@ class Product(models.Model):
         decimal_places=6, max_digits=15, null=True)
     buy_now_url = models.TextField(default=None)
     #
-    category_ids = models.ManyToManyField(Category)
+    # category_ids = models.ManyToManyField(Category)
     company_id = models.IntegerField(default=None)
     details_layout = models.TextField(default=None)
-    discount = models.TextField(default=None)
-    discount_prc = models.TextField(default=None)
+    discount = models.TextField(default=None, null=True)
+    discount_prc = models.TextField(default=None, null=True)
     # discounts = models.TextField()
     discussion_thread_id = models.TextField(default=None)
     discussion_type = models.TextField(default=None)
@@ -37,7 +36,7 @@ class Product(models.Model):
     free_shipping = models.TextField(default=None)
     has_options = models.TextField(default=None)
     height = models.TextField(default=None)
-    image_pairs = models.TextField(default=None)
+    # image_pairs = models.TextField(default=None)
     is_edp = models.TextField(default=None)
     is_op = models.TextField(default=None)
     is_oper = models.TextField(default=None)
@@ -84,3 +83,52 @@ class Product(models.Model):
     updated_timestamp = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    ITEMS_PER_PAGE = 10
+
+    
+    @classmethod
+    def sync_product(cls):
+      products = cls.get_products(page=1)
+      # def filter_product(f, d):
+        # return {k:v for k,v in d.items() if f(k,v)}
+      for item in products:
+        # import pdb; pdb.set_trace()
+        r = cls.filter_product(product=item)
+        
+        product = cls(**r)
+        product.save()
+
+    @classmethod
+    def get_response(cls, items_per_page=ITEMS_PER_PAGE, page=1):
+        session = requests.Session()
+        session.headers.update(cls.request_header())
+        r = session.get('%s/%s' % (cls.api_url(), '/products'), params={
+                        'items_per_page': items_per_page, 'page': page})
+        return r
+
+    @classmethod
+    def api_url(cls):
+        return env('CSCART_API_URL')
+
+    @classmethod
+    def request_header(cls):
+        src = '%s:%s' % (env('EMAIL'), env('SECRET_KEY'))
+        token = 'Basic %s' % base64.b64encode(src.encode('utf-8')).decode('ascii')
+        return {'Authorization': token}
+
+    @classmethod
+    def get_total_items(cls):
+        return cls.get_response(items_per_page=1).json()['params']['total_items']
+
+    @classmethod
+    def get_products(cls, items_per_page=ITEMS_PER_PAGE, page=1):
+        return cls.get_response(items_per_page, page).json()['products']
+
+    @classmethod
+    def filter_product(cls, product):
+      return dict([(k,v) for k,v in product.items() if k not in 
+        ['category_ids', 
+        'stickers','main_pair','product_features','discounts','qty_content', 
+        'image_pairs', 'promotions']])
--- api/views.py
+++ api/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from requests import RequestException
-import requests
-import base64
-import environ
-import json
+from api.models import Product
+from django.core import serializers
 
-# Create your views here.
-env = environ.Env()
-ITEMS_PER_PAGE = 10
 
 
 def index(request):
@@ -16,50 +10,7 @@ def index(request):
 
 
 def products(request):
-
-    try:
-        total_count = getTotalItems()
-        # num_of_loop = -(-total_count // ITEMS_PER_PAGE)
-        num_of_loop = 1
-        for page in range(1, num_of_loop):
-            records = getProducts(page=page)
-            for item in records:
-                saveProduct(item)
-                print(item)
-
-        return HttpResponse("Successs", content_type="application/json")
-    except RequestException as e:
-        print(e)
+    products = serializers.serialize('json', Product.objects.all())
+    return HttpResponse(products, content_type="application/json")
 
 
-def saveProduct(product):
-    if not Product.exists():
-        Product.create_table(read_capacity_units=1,
-                             write_capacity_units=1, wait=True)
-    Product.save(product)
-
-
-def getTotalItems():
-    return getResponse(items_per_page=1).json()['params']['total_items']
-
-
-def getResponse(items_per_page=ITEMS_PER_PAGE, page=1):
-    session = requests.Session()
-    session.headers.update(requestHeader())
-    r = session.get('%s/%s' % (apiUrl(), '/products'), params={
-                    'items_per_page': items_per_page, 'page': page})
-    return r
-
-
-def getProducts(items_per_page=ITEMS_PER_PAGE, page=1):
-    return getResponse(items_per_page, page).json()['products']
-
-
-def apiUrl():
-    return env('CSCART_API_URL')
-
-
-def requestHeader():
-    src = '%s:%s' % (env('EMAIL'), env('SECRET_KEY'))
-    token = 'Basic %s' % base64.b64encode(src.encode('utf-8')).decode('ascii')
-    return {'Authorization': token}
